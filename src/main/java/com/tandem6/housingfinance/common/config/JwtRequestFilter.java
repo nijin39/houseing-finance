d2c692e2883ac7fb6925a9170b101b6d87be43e1
nijin39 <nijin39@gmail.com>
2019-08-27 10:42:04 +0900

    [20190827] system.out.println --> logger

--- src/main/java/com/tandem6/housingfinance/common/config/JwtRequestFilter.java
+++ src/main/java/com/tandem6/housingfinance/common/config/JwtRequestFilter.java
@@ -39,9 +39,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             try {
                 username = jwtTokenUtil.getUsernameFromToken(jwtToken);
             } catch (IllegalArgumentException e) {
-                System.out.println("Unable to get JWT Token");
+                log.warn("Unable to get JWT Token");
             } catch (ExpiredJwtException e) {
-                System.out.println("JWT Token has expired");
+                log.warn("JWT Token has expired");
             }
         } else {
             log.warn("JWT Token does not begin with Bearer String {}",request.getRequestURI());
